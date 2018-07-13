72835e065d9aa4ecf671f47be79767b1f4a38e2a
YOUZHIFENG218 <YOUZHIFENG218@PINGAN.com.cn>
2018-07-13 17:34:36 +0800

    泡泡龙

--- build/js/views/start/defaultPage.js
+++ build/js/views/start/defaultPage.js
@@ -1,5 +1,6 @@
 import Sail from "../../libs/sail/sail";
 import StartUI from "../components/startui";
+import CMD_CONFIG from "../config";
 var playSection = require('../room/playSection.js');
 
 class defaultPage extends Sail.Page {
@@ -12,6 +13,7 @@ class defaultPage extends Sail.Page {
     onEnter () {
         //this.addChild(new ui.start.startUI);
         //this.startui = new StartUI;
+        Sail.io.emit(CMD_CONFIG.HALL.toIn,{'roomId':18},"socket")
         window.playSection1 = new playSection({
             renderData:CONFIG.balls
         })
